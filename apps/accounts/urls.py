bcb74fe55d91dda8b77b32a658f7cdad3bd3f71c
Poweredaj234 <atown1906@gmail.com>
2024-12-24 12:56:33 -0600

    a

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, RegisterView, LoginView, LogoutView, leaderboard, TransferBalanceView, UpdateHouseView, create_user_by_duke, register_form, create_user_by_duke_form
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -16,4 +17,6 @@ urlpatterns = [
     path('create-by-duke/', create_user_by_duke, name='create_user_by_duke'),
     path('register-form/', register_form, name='register_form'),
     path('create-user-by-duke/', create_user_by_duke_form, name='create_user_by_duke_form'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
